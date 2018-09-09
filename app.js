2b6dd3ba6a1e412953ee53c7fe9d6df75d9b7202
Tri <trihuynguyen14@gmail.com>
2018-09-09 00:26:02 -0400

    complete print button

---
+++ app.js
@@ -0,0 +1,62 @@
+const employeeList = [
+    {
+      name: 'Jan',
+      officeNum: 1,
+      phoneNum: '222-222-2222'
+    },
+    {
+      name: 'Juan',
+      officeNum: 304,
+      phoneNum: '489-789-8789'
+    },
+    {
+      name: 'Margie',
+      officeNum: 789,
+      phoneNum: '789-789-7897'
+    },
+    {
+      name: 'Sara',
+      officeNum: 32,
+      phoneNum: '222-789-4654'
+    },
+    {
+      name: 'Tyrell',
+      officeNum: 3,
+      phoneNum: '566-621-0452'
+    },
+    {
+      name: 'Tasha',
+      officeNum: 213,
+      phoneNum: '789-766-5675'
+    },
+    {
+      name: 'Ty',
+      officeNum: 211,
+      phoneNum: '789-766-7865'
+    },
+    {
+      name: 'Sarah',
+      officeNum: 345,
+      phoneNum: '222-789-5231'
+    }
+  ];
+
+const activeList = [];
+
+const render = function () {
+  $('#results').empty();
+  for (let i = 0; i < employeeList.length; i++) {
+      if (activeList.length > 0) {
+      for(let i = 0; i < activeList.length; i++)
+      $('#results').append(`<div class='box'>${activeList[i]}</div>`)
+          break;
+      } else
+      $('#results').
+      append(
+      `<div class='box'>${employeeList[i].name}<br> ${employeeList[i].officeNum}<br> ${employeeList[i].phoneNum} </div>`)
+      
+  }
+}
+
+
+$('.print-nav').on('click', render);
